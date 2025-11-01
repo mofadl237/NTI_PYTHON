759db5e0bfe58f2bf287741a86da8b9af63020ca
mofadl237 <mofadl237@gmail.com>
2025-11-01 14:40:52 +0200

    create branch

--- 1.py
+++ 1.py
@@ -1,27 +1,48 @@
-'''Talk In Day'''
-#1- Program Fundamental (VAR , DAtA TYPE ,Output,input , Library,small talk memory )
-#2- Condition (if , switch ,When use one )
-#3- Functions 
-#4- Loop (while , do-while ,for ,Use Any One)
+
 #5- Array - dictionary - tuples
 
 from os import strerror;
 
-#1-create Array For Servers name
-Servers_Names=[];
-for i in range(0,5):
-    serverName =input("Enter Server Name => ");
-    Servers_Names.append(serverName);
-#2- Check Array
-print(Servers_Names)
-#3- write In File 
-stream = open("ServersName",'a+');
-for serverName in Servers_Names :
-    stream.write(serverName+'\n')
-stream.close();
-#4- ReadForm fie
-stream = open("ServersName",'r+');
-content = stream.readlines()
-print(content)
-for serverName in content:
-   print(serverName)
+# 1-create Array For Servers name
+# Servers_Names=[];
+# for i in range(0,5):
+#     serverName =input("Enter Server Name => ");
+#     Servers_Names.append(serverName);
+# #2- Check Array
+# print(Servers_Names)
+# #3- write In File 
+# stream = open("ServersName",'a+');
+# for serverName in Servers_Names :
+#     stream.write(serverName+'\n')
+# stream.close();
+# #4- ReadForm fie
+# stream = open("ServersName",'r+');
+# content = stream.readlines()
+# print(content)
+# for serverName in content:
+#    print(serverName)
+
+
+
+
+
+# try:
+#     number=input("Enter Number : ");
+#     print(int(number));
+# except:
+#     number=input("Enter Number : ");
+#     print(int(number));
+
+
+
+# python file1
+# print("Alll")
+# print("Alll2")
+# print("Alll333")
+# print("Alll33333e")
+# print("Allleeee")
+# print("Allleee")
+
+
+
+
